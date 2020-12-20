f7800190d2f1920706326e575f15f403bdf1e0db
Ekaterina <ekaterina_grigoryeva97@mail.ru>
2020-12-20 19:38:48 +0700

    Complete

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = K.appName
+        
+        for family: String in UIFont.familyNames
+        {
+            print(family)
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
+            }
+        }
+    }
+    
+ 
+    
+
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '14.2'
+
+target 'Flash Chat iOS13' do
+
+  use_frameworks!
+
+pod 'CLTypingLabel'
+pod 'Firebase/Auth'
+pod 'Firebase/Firestore'
+
+
+end
